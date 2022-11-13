9b779575fcddb0ae3189a8ca2b7069662f9fb4ba
Ark-Sec <64931479+Ark-Sec@users.noreply.github.com>
2022-11-13 18:25:25 +0200

    Update mullvad.sh

--- vpn-setups/mullvad.sh
+++ vpn-setups/mullvad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mullvad account login
-mullvad relay set location se mma se-mma-002
+mullvad relay set location ch zrh ch-zrh-001
 mullvad auto-connect set on
 mullvad lan set allow
 
 mullvad dns set default --block-ads --block-malware --block-trackers 
-reset:
+#reset:
 # mullvad dns set default
