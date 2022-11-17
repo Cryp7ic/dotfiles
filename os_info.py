d4bf463e8766592442e5eea4dc0e9e8670859eae
Ark-Sec <64931479+Ark-Sec@users.noreply.github.com>
2022-11-17 13:48:15 +0200

    Add files via upload

---
+++ os_info.py
@@ -0,0 +1,21 @@
+import platform as pl
+
+os_profile = [
+        'architecture',
+        'linux_distribution',
+        'mac_ver',
+        'machine',
+        'node',
+        'platform',
+        'processor',
+        'python_build',
+        'python_compiler',
+        'python_version',
+        'release',
+        'system',
+        'uname',
+        'version',
+    ]
+for key in os_profile:
+  if hasattr(pl, key):
+    print(key +  ": " + str(getattr(pl, key)()))
