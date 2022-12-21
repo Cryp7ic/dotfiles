30efec378042ca229bc524747f4318e7327d6a94
Cryp7ic <64931479+Cryp7ic@users.noreply.github.com>
2022-12-21 11:24:22 +0200

    Delete backwards.py

--- backwards.py
+++
@@ -1,7 +0,0 @@
-with open('test2.sh') as input_file:
-    file_content = input_file.read()
-
-reverse_file_content = file_content[::-1]
-
-with open('out.txt', 'w') as output_file:
-    output_file.writelines(reverse_file_content)
